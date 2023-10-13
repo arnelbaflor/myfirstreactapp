823051b1652af43ab6a43396f98dbe9b98c840fb
arnelbaflor <131433540+arnelbaflor@users.noreply.github.com>
2023-10-13 15:21:39 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+//e157c92a OMDb API key
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello, Dio</h1>
+    <h2>POGI!!! SIGE NA?!!</h2>
+    </>
+  );
+}
+
+export default App;
